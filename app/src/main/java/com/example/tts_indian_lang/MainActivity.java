b33bb19713b576d033d01a8898d5fee99b10a6e9
Prateek <prateekj297@gmail.com>
2019-02-15 11:50:36 +0530

    Add generate text button

--- app/src/main/java/com/example/tts_indian_lang/MainActivity.java
+++ app/src/main/java/com/example/tts_indian_lang/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.tts_indian_lang;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void generate_text(View view) {
+        TextView sentenceTextView = (TextView) findViewById(R.id.textView_sentence);
+        sentenceTextView.setText("Hello World!");
+
     }
 }
