7cdc67ee50a7dbdf36b08cea2010d10a071eb123
Mohit Verma <mohitxlnct@gmail.com>
2019-02-16 13:59:43 +0530

    something

--- app/src/main/java/com/example/tts_indian_lang/MainActivity.java
+++ app/src/main/java/com/example/tts_indian_lang/MainActivity.java
@@ -1,38 +1,129 @@
 package com.example.tts_indian_lang;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.TextureView;
+import android.view.InputDevice;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView textview_sentence;
+    SpeechRecognizer speechrecognizer;
+    Intent speechrecognizerintent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkPermission();
+
+
+        textview_sentence = findViewById(R.id.button_submit);
+
+        speechrecognizer  = SpeechRecognizer.createSpeechRecognizer(this);
+        Intent speechrecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechrecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        speechrecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,
+                Locale.getDefault());
+
+        speechrecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle params) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onRmsChanged(float rmsdB) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] buffer) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int error) {
+
+            }
+
+            @Override
+            public void onResults(Bundle results) {
+
+            }
+
+            @Override
+            public void onPartialResults(Bundle partialResults) {
+
+            }
+
+            @Override
+            public void onEvent(int eventType, Bundle params) {
+
+            }
+        });
+
+
+        findViewById(R.id.button_submit).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                switch (event.getAction()) {
+                    case event.ACTION_UP:
+                        textview_sentence.setHint("You will see the text here");
+                        break;
+
+                    case event.ACTION_DOWN:
+                        textview_sentence.setHint("Listening");
+                        break;
+
+                }
+                return false;
+            }
+        });
+    private void checkPermission()
+        {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            {
+                if(!(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED))
+                {
+                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:" + getPackageName()));
+                startActivity(intent);
+                finish();
+                }
+            }
+        }
+
     }
 
-    public void generate_text(View view) {
-        TextView sentenceTextView = (TextView) findViewById(R.id.textView_sentence);
-        String[] sentence=new String[10];
-        int i;
-        //for(i=0;i<10;i++){
-        //    sentence[i]="Hello "+i;
-        //}
-        sentence[0]="भारत ने पाकिस्तान से छीना 'मोस्ट फेवर्ड नेशन' का दर्जा, जानिए क्या होगा इसका असर";
-        sentence[1]="पुलवामा हमले पर सोनिया गांधी को आया गुस्सा, बोलीं-उम्मीद है, दोषी दंडित किए जाएंगे |";
-        sentence[2]="अमेरिका ने पुलवामा में आतंकवादी हमले की निंदा की, दिया ये बड़ा बयान";
-        sentence[3]="वंदे भारत एक्सप्रेस 180 किमी की रफ्तार से दौड़ी, टिकटों की बुकिंग शुरू";
-        sentence[4]="Gully Boy Box Office: रणवीर सिंह ने 'गली बॉय' से किया धमाल, पहले दिन कमाए इतने करोड़";
-        sentence[5]="इसलिए मारिया शारापोवा बीएनबी परिबास टूर्नामेंट से हुईं बाहर";
-        sentence[6]="नोएडा: महिला ने पति के खिलाफ दर्ज कराया अप्राकृतिक यौनाचार का मामला";
-        sentence[7]="लड़की के साथ छेड़छाड़ का किया विरोध तो गोली मारकर कार से हो गए फरार, पुलिस ने एक घंटे में ऐसे दबोचा";
-        sentence[8]="Valentine Week 2019: 8 फरवरी को प्रपोज़ डे, तो 9 को है चॉकलेट डे, पढ़ें Valentine's Day से जुड़े फेक्ट...";
-        sentence[9]="नई जनरेशन 2019 होंडा सिविक की जानकारी आई सामने, आगामी हफ्तों में लॉन्च संभव";
-        i=(int)(Math.random()*10);
-        sentenceTextView.setText(sentence[i]);
-
+    private void checkPermission() {
     }
 }
