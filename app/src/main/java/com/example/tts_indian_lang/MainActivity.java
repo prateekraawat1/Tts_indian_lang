f536f8a543620595dba6941dbaa36e06a833e691
Mohit Verma <mohitxlnct@gmail.com>
2019-02-16 12:02:34 +0530

    Permissions
    Permission

--- app/src/main/java/com/example/tts_indian_lang/MainActivity.java
+++ app/src/main/java/com/example/tts_indian_lang/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.tts_indian_lang;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.InputDevice;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textview_sentence;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textview_sentence = findViewById(R.id.button_submit);
+        findViewById(R.id.button_submit).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                switch (event.getAction())
+                {
+                    case event.ACTION_UP:
+                        textview_sentence.setHint("You will see the text here");
+                        break;
+
+                    case event.ACTION_DOWN:
+                        textview_sentence.setHint("Listening");
+                        break;
+
+                }
+                return false;
+            }
+        });
     }
 }
