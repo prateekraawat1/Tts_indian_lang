697360bd1401d8ea131385a597cbfbf92bb40005
Abhishek Jain <poonamjain840@gmail.com>
2019-02-16 12:36:23 +0530

    SPLASH

--- app/src/main/java/com/example/tts_indian_lang/MainActivity.java
+++ app/src/main/java/com/example/tts_indian_lang/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.tts_indian_lang;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.TextureView;
@@ -7,11 +9,20 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 
     public void generate_text(View view) {
