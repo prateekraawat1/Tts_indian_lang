b34a55be9aa8583e13367a8ed885365e1ad1334c
Abhishek Jain <poonamjain840@gmail.com>
2019-02-18 15:41:21 +0530

    first run slide screen added

--- app/src/main/java/com/example/tts_indian_lang/MainActivity.java
+++ app/src/main/java/com/example/tts_indian_lang/MainActivity.java
@@ -2,24 +2,66 @@ package com.example.tts_indian_lang;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import static java.lang.Boolean.getBoolean;
+
+
 public class MainActivity extends AppCompatActivity {
+    SharedPreferences prefs=null;
     private static int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
-                startActivity(homeIntent);
-                finish();
-            }
-        }, SPLASH_TIME_OUT);
+        prefs = getSharedPreferences("com.example.tts_indian_lang", MODE_PRIVATE);
+        /*if(isFirstRun==true) {
+            getSharedPreferences("PREFERENCE", MODE_PRIVATE).edit().putBoolean("isFirstRun", false).apply();
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent homeIntent = new Intent(MainActivity.this, SlideActivity.class);
+                    startActivity(homeIntent);
+                    finish();
+                }
+            }, SPLASH_TIME_OUT);
+        }
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(homeIntent);
+                    finish();
+                }
+            }, SPLASH_TIME_OUT);*/
+    }
+    protected void onResume(){
+        super.onResume();
+        if (prefs.getBoolean("firstrun", true)) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent homeIntent = new Intent(MainActivity.this, SlideActivity.class);
+                    startActivity(homeIntent);
+                    finish();
+                }
+            }, SPLASH_TIME_OUT);
+            prefs.edit().putBoolean("firstrun", false).commit();
+        }
+        else if (!(prefs.getBoolean("firstrun", true))) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(homeIntent);
+                    finish();
+                }
+            }, SPLASH_TIME_OUT);
+        }
     }
 }
---
+++ app/src/main/java/com/example/tts_indian_lang/SlideAdapter.java
@@ -0,0 +1,92 @@
+package com.example.tts_indian_lang;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+public class SlideAdapter extends PagerAdapter {
+
+    Context context;
+    LayoutInflater inflater;
+
+    public SlideAdapter(Context context){
+        this.context=context;
+    }
+
+    //Array
+    public int[] list_images={
+
+            R.drawable.phone,
+            R.drawable.flight,
+            R.drawable.bus,
+            R.drawable.train
+    };
+
+    public String[] list_title={
+
+            "Phone",
+            "Flight",
+            "Bus",
+            "Train"
+    };
+
+    public String[] list_description={
+
+            "Big discounts on Smart Phones",
+            "Upto 25% off on Domestic Flights",
+            "Enjoy Travelling on bus with flat 100 off",
+            "10% cashback on first train booking"
+    };
+    public int[] list_color={
+
+            Color.rgb(193, 66, 44),
+            Color.rgb(193, 172, 44),
+            Color.rgb(193, 41, 249),
+            Color.rgb(68, 83, 242)
+
+    };
+
+    @Override
+    public int getCount() {
+        return list_title.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object obj) {
+        return view==(LinearLayout)obj;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+
+        inflater = (LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.slide,container,false);
+
+        LinearLayout linearLayout = (LinearLayout)view.findViewById(R.id.slidelinearlayout);
+        ImageView img = (ImageView)view.findViewById(R.id.slideimg);
+        TextView txt1 = (TextView) view.findViewById(R.id.slidetitle);
+        TextView txt2 = (TextView) view.findViewById(R.id.slidedescription);
+
+
+        img.setImageResource(list_images[position]);
+        txt1.setText(list_title[position]);
+        txt2.setText(list_description[position]);
+        linearLayout.setBackgroundColor(list_color[position]);
+
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((LinearLayout)object);
+    }
+}
