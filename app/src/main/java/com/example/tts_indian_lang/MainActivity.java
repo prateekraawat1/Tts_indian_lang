bfc25754afb4b17f5e847793779cdc8a38fd33e6
himanshume <39821335+himanshume@users.noreply.github.com>
2019-02-19 20:42:08 +0530

    adjusted splash_screen
    removed actionbar and text"Welcome to bharat bhasha"

--- app/src/main/java/com/example/tts_indian_lang/MainActivity.java
+++ app/src/main/java/com/example/tts_indian_lang/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 import static java.lang.Boolean.getBoolean;
 
